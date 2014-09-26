e52ba54e07c971744d862df8196631ad993e1ffb
孙全军 <quanjun1@staff.sina.com.cn>
2014-09-26 16:32:59 +0800

    init

---
+++ project/admin.monitor.weibo.cn/userInfo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by Zend Studio
+ * User: dongxing
+ * Date: 15-09-26
+ * Time: 上午11:10
+ */
+
+//本文件所在路径
+//define('ABS_CUR_LOGIN_DIR', dirname(__FILE__) . '/');
+
+require_once("./userInfo.phpconf/conf.php");
+require_once($moduleShowtmpl);
+?>
